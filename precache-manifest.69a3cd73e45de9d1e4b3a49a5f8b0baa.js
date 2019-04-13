b7ab3272fb8630934ba4c4dcb728abc82bfacf99
Oscar Paesi <oscar.paesi@gmail.com>
2019-04-12 22:46:40 -0300

    Updates

---
+++ precache-manifest.69a3cd73e45de9d1e4b3a49a5f8b0baa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6afbab892675c27b2e6af81a6df4a996",
+    "url": "/fend-my-reads/static/media/arrow-drop-down.6afbab89.svg"
+  },
+  {
+    "revision": "5e29d74bb92fafc7d8d2b808de612408",
+    "url": "/fend-my-reads/static/media/arrow-back.5e29d74b.svg"
+  },
+  {
+    "revision": "9303ba147509161c9708d5bc3060ffd8",
+    "url": "/fend-my-reads/static/media/add.9303ba14.svg"
+  },
+  {
+    "revision": "14a7ae846e5d3914f490",
+    "url": "/fend-my-reads/static/js/runtime~main.07cf9dd4.js"
+  },
+  {
+    "revision": "7eca890989e439b51bdc",
+    "url": "/fend-my-reads/static/js/main.5fd1a873.chunk.js"
+  },
+  {
+    "revision": "6ba253797a04e82d2fd3",
+    "url": "/fend-my-reads/static/js/2.8fd270b2.chunk.js"
+  },
+  {
+    "revision": "7eca890989e439b51bdc",
+    "url": "/fend-my-reads/static/css/main.3b799d19.chunk.css"
+  },
+  {
+    "revision": "349f699c9e9b9524004c2d7205d83899",
+    "url": "/fend-my-reads/index.html"
+  }
+];
